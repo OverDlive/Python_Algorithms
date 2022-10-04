b7520a14b51cd36971735f58a65455ab62ae7661
OverDlive <ss731239@gmail.com>
2022-10-04 20:10:58 +0900

    10.04

---
+++ 시각.py
@@ -0,0 +1,18 @@
+import time
+
+start_time = time.time()
+
+h = int(input())
+count = 0
+
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
+
+end_time = time.time()
+
+print('time:', end_time-start_time)
